3f454da6111a2931872cb5182dd0901587f3c3f7
cprevozn <42843954+cprevozn@users.noreply.github.com>
2018-08-30 16:31:39 -0400

    Add files via upload

---
+++ 8_30_18_Day1.py
@@ -0,0 +1,62 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[3]:
+
+
+print(Hello World!)
+
+
+# In[4]:
+
+
+print('"Hello World!"')
+
+
+# In[5]:
+
+
+print('hw')
+
+
+# In[6]:
+
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by pyhton.
+
+print("I could have code like this.")  # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print ("This won't run.")
+
+print("This will run.")
+
+
+# In[7]:
+
+
+don't be messing around
+just be yourself
+
+
+# don't be messing around
+# just be yourself
